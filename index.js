a7cd3e4aed6eb46daebb04cc4972dd625928cb64
Hillary Jackson <hill_a_ry@hotmail.com>
2023-08-09 17:23:54 -0400

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const cards =[]
+    for (let i=0; i < names.length; i++){
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+    return cards;
+}
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(num){
+    let i=num;
+    while (i >= 0){
+        console.log(i);
+        --i;
+    }
+}
+
+countDown(10);
